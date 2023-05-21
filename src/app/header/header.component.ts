de3c4842abf673019494bfe5b88d1e5f53eb034f
brady-619 <daniel.montano@dish.com.mx>
2023-05-21 12:44:48 -0600

    beta 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
+
+  countrycode:string ="52"; 
+  whatsappnumber:string ="5513794083";
+  url:string= "https://wa.me/" +this.countrycode+this.whatsappnumber+"?text=¡Hola! Podrías ayudarme con una duda.";
+  
+
   constructor() { }
 
   ngOnInit() {}
